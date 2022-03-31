452e98c28c6ed60a85f9d9efb74964764e36b194
gretazas <esminis@yahoo.com>
2022-03-31 13:34:02 +0000

    remove the button with the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 function winTheGame(){
 document.getElementById('winner').innerHTML = ``
 document.getElementById('winner').innerHTML = `
-    <h1>CONGRADULATIONS!</h1>
-    <p>You just have won the whole game</p>
+    <h1>CONGRATULATIONS!</h1>
+    <p>You have just won the whole game.</p>
     <h6>You are the winner!</h6>
+ 
+`
+document.getElementById('button').remove();
+document.body.innerHTML += `
+<div>
+<p style="text-align:center; font-size:16px; color:red">Happy April Fool's Day</p>
+</div>
 `
 }
 document.getElementById('button').addEventListener('click', winTheGame)

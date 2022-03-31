c5c6c57bb535fefa522c4ba33cca100b470214c5
gretazas <esminis@yahoo.com>
2022-03-31 13:12:29 +0000

    add function into the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console. log("Hello World");
+function winTheGame(){
+document.getElementById('winner').innerHTML = ``
+document.getElementById('winner').innerHTML = `
+    <h1>CONGRADULATIONS!</h1>
+    <p>You just have won the whole game</p>
+    <h6>You are the winner!</h6>
+`
+}
+document.getElementById('button').addEventListener('click', winTheGame)
